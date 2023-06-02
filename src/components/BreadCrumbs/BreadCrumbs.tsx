c80ae97a1af7d0e479b7f71d40270469d3b656ec
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-06-02 20:09:29 +0300

    feat: add crumbs component

---
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.scss"
+import { crumbInfo } from "../../pages/Details/Details";
+
+interface BreadCrumbsProps {
+  prev?: string,
+  current: crumbInfo,
+  goBack: () => void
+
+}
+
+const BreadCrumbs = ({prev, current, goBack}: BreadCrumbsProps) => {
+  const { product, mark, year } = current
+  return (
+    <div className="crumbs">
+      <p className="crumbs__item" onClick={goBack}>назад</p>
+      <p>/</p>
+      <p className="crumbs__item crumbs-disabled">{product} {mark} {year}</p>
+    </div>
+  );
+};
+
+export default BreadCrumbs;
