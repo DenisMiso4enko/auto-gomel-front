cc0a1a13c118f537f3405f9bdbcb5308564937b0
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-06-04 13:47:19 +0300

    feat: add popular marks slider

--- src/components/PopularMarks/PopularMarks.tsx
+++ src/components/PopularMarks/PopularMarks.tsx
@@ -1,19 +1,55 @@
-import SectionTitle from '../SectionTitle/SectionTitle'
-import PopularMarksCard from '../PopularMarksCard/PopularMarksCard'
-import { marksData } from './marksData'
+import SectionTitle from "../SectionTitle/SectionTitle";
+import PopularMarksCard from "../PopularMarksCard/PopularMarksCard";
+import { marksData } from "./marksData";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
 
 const PopularMarks = () => {
 
   return (
     <div className="section marks container">
       <SectionTitle title="Поиск по модели" />
-      <div className="marks__list">
-        {marksData?.map((mark) => (
-          <PopularMarksCard key={mark.title} {...mark} />
-        ))}
-      </div>
-    </div>
-  )
-}
+      <Swiper
+        loop={true}
+        slidesPerView={6}
+        spaceBetween={30}
+        pagination={{
+          clickable: true
+        }}
+        autoplay={{
+          delay: 1000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+          stopOnLastSlide: true
 
-export default PopularMarks
+        }}
+        modules={[Pagination, Autoplay ]}
+        className="mySwiper"
+        breakpoints={{
+          320: {
+            slidesPerView: 2
+          },
+          768: {
+            slidesPerView: 4
+          },
+          1080: {
+            slidesPerView: 6
+          }
+        }}
+      >
+        {marksData?.map((mark) => (
+          <SwiperSlide key={mark.title}>
+            <PopularMarksCard key={mark.title} {...mark} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default PopularMarks;
