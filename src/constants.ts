bbffedb5e31d981e0aed69b2aaa1197102d5dfc4
Ekimov  Denis <ekimoffdenis@gmail.com>
2023-06-11 18:15:38 +0300

    fix: fixed .env variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const PATHDOMAIN = 'http://localhost:8888'
-
-// @ts-ignore
-// export const PATHDOMAIN = import.meta.env.VITE_BASE_URL;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+export const PATHDOMAIN =
+import.meta.env.VITE_IS_PROD === 'yes'
+  ? import.meta.env.VITE_PROD_BASE_URL
+  : import.meta.env.VITE_DEV_BASE_URL
